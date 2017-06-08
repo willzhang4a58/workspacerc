fe4e0545f18e1f7421c33cba8f49440295d2af92
willzhang4a58 <willzhang4a58@gmail.com>
2017-06-08 13:38:00 +0800

    update include guartd

---
+++ .bin/update_include_guard.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+def UpdateGuard(file_path, new_guard):
+  cont_lines = open(file_path).readlines()
+  # find old_guard
+  old_guard = ""
+  for i in range(0, len(cont_lines) - 1):
+    if cont_lines[i].startswith('#ifndef'):
+      old_guard_candidate = cont_lines[i][7:].strip()
+      if cont_lines[i+1].startswith('#define') == False:
+        continue
+      old_guard_check = cont_lines[i+1][7:].strip()
+      if old_guard_candidate == old_guard_check:
+        old_guard = old_guard_candidate
+        break
+  # replace
+  for i in range(0, len(cont_lines)):
+    cont_lines[i].replace(old_guard, new_guard)
+  # write
+  fp = open(file_path, 'w')
+  for line in cont_lines:
+    fp.write(line)
+  fp.close()
+
+
+  #fp = open(file_path, 'w')
+
+if __name__ == '__main__':
+  prefix = ('_'.join(sys.argv[1:]) + '_').upper()
+  for root, dirs, files in os.walk('.'):
+    for file_name in files:
+      file_path = root + '/' + file_name
+      guard = prefix + '_'.join([root, file_name])[2:].replace('.', '_').upper()
+      UpdateGuard(file_path, guard)
