23da2c5f8a20272f4f39cf6af46e80bad3438d33
willzhang <willzhang@willzhangdeMacBook-Pro.local>
2019-05-08 13:35:19 +0800

    fix bug

--- .tmp/.install.sh
+++ .tmp/.install.sh
@@ -3,4 +3,3 @@ git remote add origin https://github.com/willzhang4a58/workspacerc.git
 git pull origin master
 echo "\$include ~/.myinputrc" >> .inputrc
 echo "source ~/.mybashrc" >> .bashrc
-source ~/.mybashrc
