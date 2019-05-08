80cd55ab6a2d3508b49081781859b7766e299e54
willzhang <willzhang@willzhangdeMacBook-Pro.local>
2019-05-08 13:30:53 +0800

    bind -f inputrc

--- .tmp/.install.sh
+++ .tmp/.install.sh
@@ -1,5 +1,6 @@
 git init
 git remote add origin https://github.com/willzhang4a58/workspacerc.git
 git pull origin master
-echo "source .mybashrc" >> .bashrc
-echo "\$include .myinputrc" >> .inputrc
+echo "\$include ~/.myinputrc" >> .inputrc
+echo "source ~/.mybashrc" >> .bashrc
+source ~/.mybashrc
